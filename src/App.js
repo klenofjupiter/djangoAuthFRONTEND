c24283e88db5c5204bd9ded22c5e9513873a10d2
Leigh <lsteiner@locus.co>
2019-03-06 14:47:35 -0500

    dummy data list render

--- src/App.js
+++ src/App.js
@@ -2,24 +2,37 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+const list = [ {
+    "id":1,
+    "title":"1st todo",
+    "body":"Learn Django properly."
+}, {
+    "id":2,
+    "title":"Second item",
+    "body":"Learn Python."
+}, {
+    "id":3,
+    "title":"Learn HTTP",
+    "body":"It's important."
+} ]
+
+
 class App extends Component {
+  state ={ list }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <ol>
+          {
+            this.state.list.map( item => (
+              <li key={item.id}>
+                <b>{item.title}</b>:   <span>{item.body}</span> 
+              </li>
+             )
+            )
+          }
+        </ol>
       </div>
     );
   }
